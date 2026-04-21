df76c530aa4bf18ed7ac082ebf4ad9e4c844c3d8
Max Kellermann <max.kellermann@gmail.com>
2026-04-21 08:17:16 +0200

    test/util/TestUriUtil: add uri_safe_local() test

--- test/util/TestUriUtil.cxx
+++ test/util/TestUriUtil.cxx
@@ -8,6 +8,38 @@
 
 using std::string_view_literals::operator""sv;
 
+TEST(UriUtil, SafeLocal)
+{
+	EXPECT_TRUE(uri_safe_local("foo"));
+	EXPECT_TRUE(uri_safe_local("foo/bar"));
+	EXPECT_TRUE(uri_safe_local("a/b/c/d/e"));
+
+	// must be non-empty
+	EXPECT_FALSE(uri_safe_local(""));
+
+	// must not begin or end with a slash
+	EXPECT_FALSE(uri_safe_local("/"));
+	EXPECT_FALSE(uri_safe_local("/foo"));
+	EXPECT_FALSE(uri_safe_local("foo/"));
+
+	// no double slash
+	EXPECT_FALSE(uri_safe_local("foo//bar"));
+	EXPECT_FALSE(uri_safe_local("foo///bar"));
+
+	// no "." or ".." segments
+	EXPECT_FALSE(uri_safe_local("./foo"));
+	EXPECT_FALSE(uri_safe_local("../foo"));
+	EXPECT_TRUE(uri_safe_local("foo/.bar"));
+	EXPECT_FALSE(uri_safe_local("foo/.."));
+	EXPECT_FALSE(uri_safe_local("foo/."));
+	EXPECT_FALSE(uri_safe_local("foo/../bar"));
+	EXPECT_FALSE(uri_safe_local("foo/./bar"));
+	EXPECT_TRUE(uri_safe_local(".foo"));
+	EXPECT_TRUE(uri_safe_local("..foo"));
+	EXPECT_TRUE(uri_safe_local("...foo"));
+	EXPECT_TRUE(uri_safe_local(".....foo"));
+}
+
 TEST(UriUtil, RemoveAuth)
 {
 	EXPECT_EQ(uri_remove_auth("http://www.example.com/"),
